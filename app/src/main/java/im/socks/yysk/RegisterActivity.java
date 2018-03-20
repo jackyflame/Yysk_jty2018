927d268b506bbdc8dad68f43aa995ec4a0e28bad
Haozi <263667227@qq.com>
2018-03-21 00:47:08 +0800

    增加登记注册页面

---
+++ app/src/main/java/im/socks/yysk/RegisterActivity.java
@@ -0,0 +1,23 @@
+package im.socks.yysk;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Android Studio.
+ * ProjectName: Yysk_jty2018
+ * Author: Haozi
+ * Date: 2018/3/20
+ * Time: 22:49
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist);
+    }
+
+}
---
+++ app/src/main/java/im/socks/yysk/SplashActivity.java
@@ -0,0 +1,67 @@
+package im.socks.yysk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by Android Studio.
+ * ProjectName: Yysk_jty2018
+ * Author: Haozi
+ * Date: 2018/3/20
+ * Time: 20:37
+ */
+
+public class SplashActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Runnable runnable;
+    private int SPLASH_DURATION = 2500;
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        findViewById(R.id.btn_regist).setOnClickListener(this);
+        findViewById(R.id.btn_login).setOnClickListener(this);
+    }
+
+    private void postRunnable(final long delay) {
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                enterNextActivity();
+            }
+        };
+        mHandler.postDelayed(runnable, delay);
+    }
+
+    private void removeRunnable() {
+        if (runnable != null && mHandler != null) mHandler.removeCallbacks(runnable);
+    }
+
+    private void enterNextActivity() {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        removeRunnable();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_regist:
+                startActivity(new Intent(this,RegisterActivity.class));
+                break;
+            case R.id.btn_login:
+                startActivity(new Intent(this,LoginActivity.class));
+                break;
+        }
+    }
+}
