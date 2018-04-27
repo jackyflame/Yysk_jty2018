a68e0620f1448df6b310d491210d1fef4b095f00
Haozi <263667227@qq.com>
2018-04-27 22:45:11 +0800

    更新分享Lib库；

---
+++ jsharelib/src/main/java/com/socks/yyskjtyqy/wxapi/WXEntryActivity.java
@@ -0,0 +1,13 @@
+package com.socks.yyskjtyqy.wxapi;
+
+import cn.jiguang.share.wechat.WeChatHandleActivity;
+
+/**
+ * Created by Android Studio.
+ * ProjectName: Yysk_jty2018
+ * Author: Haozi
+ * Date: 2018/4/27
+ * Time: 21:56
+ */
+public class WXEntryActivity extends WeChatHandleActivity{//extends WeChatHandleActivity
+}
