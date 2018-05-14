37225d4d5b5e0e64ca94901c33fd89814f97ef1e
yinhao <263667227@qq.com>
2018-05-14 14:43:16 +0800

    更新YJS的url全部更新为域名

--- app/src/qy/java/im/socks/yysk/vpn/VpnConfig.java
+++ app/src/qy/java/im/socks/yysk/vpn/VpnConfig.java
@@ -8,9 +8,9 @@ public class VpnConfig {
 
     public final static boolean BYPASS_CHINA_DEFAULT = true;
 
-    public final static String API_URL_DEFAULT = "http://117.23.50.120:8080/ApiServer/SsrHandle?Msg=";
+    public final static String API_URL_DEFAULT = "http://api.1jiasu.com:8080/ApiServer/SsrHandle?Msg=";
 
-    public final static String API_SHARE_URL = "http://117.23.50.120:90/#/register?invite_code=";
+    public final static String API_SHARE_URL = "https://admin.1jiasu.com/#/register?invite_code=";
     public final static String API_SHAREIMAGE_URL = "https://api.1jiasu.com/media/images/ic_launcher.png";
 
 }
