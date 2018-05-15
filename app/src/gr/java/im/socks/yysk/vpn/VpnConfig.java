e01770b3f189ec4003f334850ea2957a28265b11
haozi <yh16266@126.com>
2018-05-15 21:40:46 +0800

    更新配置

--- app/src/gr/java/im/socks/yysk/vpn/VpnConfig.java
+++ app/src/gr/java/im/socks/yysk/vpn/VpnConfig.java
@@ -11,5 +11,4 @@ public class VpnConfig {
     public final static String API_URL_DEFAULT = "http://api.wanpas.com:8080/wanpas/SsrHandle?Msg=";
 
 //    public final static String API_URL_DEFAULT = "http://103.242.135.205:8080/wanpas/SsrHandle?Msg=";
-
 }
