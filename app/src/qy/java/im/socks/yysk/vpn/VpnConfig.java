9b33911fb15c13b022114da8c46b7c7d59f9a08c
Haozi <yh16266@126.com>
2018-05-24 00:54:12 +0800

    更新分享邀请链接；

--- app/src/qy/java/im/socks/yysk/vpn/VpnConfig.java
+++ app/src/qy/java/im/socks/yysk/vpn/VpnConfig.java
@@ -10,7 +10,7 @@ public class VpnConfig {
 
     public final static String API_URL_DEFAULT = "http://api.1jiasu.com:8080/ApiServer/SsrHandle?Msg=";
 
-    public final static String API_SHARE_URL = "https://admin.1jiasu.com/#/register?invite_code=";
+    public final static String API_SHARE_URL = "https://reg.1jiasu.com/register?invite_code=";
     public final static String API_SHAREIMAGE_URL = "https://api.1jiasu.com/media/images/ic_launcher.png";
 
 }
